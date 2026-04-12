33848445980c7dcae48b07b05d03934142989014
Swetha <swetha.nv7@gmail.com>
2026-04-12 17:15:40 -0400

    1A-setup

---
+++ cart-service/src/test/java/com/microservices/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
